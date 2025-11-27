13df67bc6f97bec46e162c039be1e1105f98df27
BrunoPisfil <brunosp1100@gmail.com>
2025-11-27 00:33:08 -0500

    a

---
+++ storage/framework/views/05f7db9131d010987c443ea9e8f081fa.php
@@ -0,0 +1,16 @@
+<?php $__env->startSection('title', 'Dashboard'); ?>
+
+<?php $__env->startSection('content_header'); ?>
+    <h1>PROYECTITO</h1>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+    <p>EN DESARROLLO</p>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('css'); ?>
+    
+    
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('adminlte::page', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Pc\Documents\PryRsuReciclaje\resources\views/admin/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/1ebc3a2162208f06d75d5e9effb90bb1.php
@@ -0,0 +1,4 @@
+<a href="/">
+    <img src="<?php echo e(asset('images/logo-muni-jlo.png')); ?>" alt="Logo" class=" object-contain" />
+</a>
+<?php /**PATH C:\Users\Pc\Documents\PryRsuReciclaje\resources\views/components/authentication-card-logo.blade.php ENDPATH**/ ?>
